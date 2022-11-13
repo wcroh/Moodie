9101b75db756d88cb766b8f75012a78603fe2500
wcroh <95352721+wcroh@users.noreply.github.com>
2022-11-13 18:46:23 +0900

    fix: nginx static files serving && pyc files delete, docker-compose add collectstatic

--- src/Moodie/settings.py
+++ src/Moodie/settings.py
@@ -117,11 +117,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+# html에서 {% static '~~'%} 할때 사용
 STATIC_URL = '/static/'
+# 프로젝트 전반적으로 사용되는 static 경로 설정
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    #os.path.join(BASE_DIR, 'static/')
 ]
-STATIC_ROOT = os.path.join("staticfiles")
+# python manage.py collectstatic 할 때 어디로 모을지 설정
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
